033c0225a40cca4c2b19ec912eea3fef2d8a69e7
Chan Nyein Thaw <chanyeinthaw@gmail.com>
2019-01-08 23:04:44 +0630

    Tests

--- src/test/java/TestLoadNetwork.java
+++ src/test/java/TestLoadNetwork.java
@@ -1,15 +1,17 @@
 import com.channyeinthaw.classification.psonn.NNSwarmBuilder;
 import com.channyeinthaw.classification.psonn.Network;
 import com.channyeinthaw.classification.psonn.TrainingSample;
-import com.channyeinthaw.optimization.pso.Particle;
 import com.channyeinthaw.optimization.pso.Swarm;
 import com.channyeinthaw.util.Log;
+import com.channyeinthaw.util.Logger;
 
 import java.io.IOException;
 import java.util.Arrays;
 
-public class TestNetwork {
+public class TestLoadNetwork {
     public static void main(String[] args) throws IOException {
+        Log.setLogger(new Logger(Logger.Type.Console, "PSONN"));
+
         TrainingSample[] samples = new TrainingSample[]{
                 new TrainingSample("0,1,0", 1, 2),
                 new TrainingSample("0,1,0", 1, 2),
@@ -17,21 +19,19 @@ public class TestNetwork {
                 new TrainingSample("1,0,0", 1, 2),
         };
 
-        NNSwarmBuilder b = new NNSwarmBuilder(30);
-        b.setNetworkSchema(() -> Network.build(2,1));
-        b.setObjectiveFunction(p -> {
+        Swarm s = new Swarm("C:\\Users\\chany\\Desktop\\pso\\iter_20.pso", p -> {
             Network n = Network.build(2,1);
             n.updateWeightsAndBias(p);
             return n.computeMSE(samples);
         });
 
-        Swarm swarm = b.build(0.4, 0.1);
-        swarm.setSaveDir("C:\\Users\\chany\\Desktop\\pso");
-        swarm.run(0.00000001, 0.9, 0.303);
+        s.setSaveDir("C:\\Users\\chany\\Desktop\\pso1");
+        s.run(0.00000001, 0.9, 0.303);
 
-        for(Network n: b.getBestNetworks()) {
-            for(TrainingSample s: samples)
-                System.out.println(Arrays.toString(n.compute(s.getInputs())));
+
+        for(Network n: NNSwarmBuilder.getBestNetworks(s, () -> Network.build(2,1))) {
+            for(TrainingSample ss: samples)
+                System.out.println(Arrays.toString(n.compute(ss.getInputs())));
 
             Log.divider();
         }
